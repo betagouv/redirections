7526cead062a9d836a7a1ed3046d64baa77da081
pli01 <pli01@users.noreply.github.com>
2022-01-13 09:25:56 +0100

    Ajout Makefile : make -C .ci test

--- .ci/test-erb-nginx-conf.sh
+++ .ci/test-erb-nginx-conf.sh
@@ -4,19 +4,28 @@
 set -euo pipefail
 
 export LC_ALL=C
-apt-get -qy update
-apt-get -qy install ruby
+apt-get -qqqy update
+apt-get -qqqy install ruby
 
 cd $(dirname $0)
 
-# test erb syntax
-( erb -P -x -T '-'  servers.conf.erb  | ruby -c ) || exit $?
+nginx_servers_erb="servers.conf.erb"
+nginx_servers_conf="$(basename $nginx_servers_erb .erb)"
 
+echo "# test $nginx_servers_erb erb syntax"
+if [ ! -f "$nginx_servers_erb" ] ; then
+    echo "ERROR: $nginx_servers_erb not found"
+    exit 1
+fi
+( erb -P -x -T '-' $nginx_servers_erb | ruby -c ) || exit $?
+
+echo "# generate nginx $nginx_servers_conf file"
 # test nginx syntax
 export URL_STANDUP="${URL_STANDUP:-http://test}"
 export PORT=${PORT:-80}
-erb servers.conf.erb > /etc/nginx/conf.d/servers.conf
+erb $nginx_servers_erb > /etc/nginx/conf.d/$nginx_servers_conf
 
+echo "# test nginx $nginx_servers_conf syntax"
 nginx -t -c /etc/nginx/nginx.conf 2>&1 || exit $?
 
 echo "Test OK"
